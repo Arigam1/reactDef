5f610b370516ec94115cec48370bf1c498896eb9
unknown <yebkobvsebolshe@gmail.com>
2023-05-31 20:06:53 +0400

    comp with classes

---
+++ src/App.js
@@ -0,0 +1,36 @@
+import React from 'react'
+import Header from './components/Header'
+import Image from './components/Image'
+import logo from './img/logo.jpg'
+
+
+
+class App extends React.Component {
+  helpText = 'Help text!'
+
+  render() {
+    return (
+      <div className='name'>
+        <Header title="Шапка сайта!!!"/>
+        <h1>{this.helpText}</h1>
+        <input
+          placeholder={this.helpText}
+          onClick={this.inputClick}
+          onMouseEnter={this.mouseOver}
+        />
+        <p>{this.helpText === 'Help text!' ? 'Yes' : 'No'}</p>
+        <Image image={logo}/>
+        <img src={logo}/>
+      </div>
+    )
+  }
+
+  inputClick() {
+    console.log('Clicked')
+  }
+  mouseOver() {
+    console.log('Mouse Over')
+  }
+}
+
+export default App
--- src/index.js
+++ src/index.js
@@ -1,24 +1,7 @@
-import React from "react";
-import * as ReactDOMClient from "react-dom/client";
+import React from 'react'
+import * as ReactDOMClient from 'react-dom/client'
+import App from './App'
+import './css/main.css'
 
-const inputClick = () => console.log("Clicked")
-const mouseOver = () => console.log("Mouse Over")
-
-const helpText = "Help text"
-
-const elements = (<div className="name">
-    <h1>{helpText}</h1>
-    <input placeholder ={helpText}
-        onClick={inputClick} onMouseEnter={mouseOver}/>
-    <p>{helpText === "Help text!" ? "Yes" : "No"}</p>
-</div>)
-
-const app = ReactDOMClient.createRoot(document.getElementById("app"))
-app.render(elements)
-
-
-// ReactDOM.render(React.createElement('input', {
-//     placeholder: 'help',
-//     onClick:() => console.log('Click'),
-//     onMouseEnter: () => console.log('Mouse Over')
-// }), document.getElementById('app'))
+const app = ReactDOMClient.createRoot(document.getElementById('app'))
+app.render(<App />)
