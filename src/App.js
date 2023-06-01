270294f8ccd72d43b713e1506dbe33ce94b57c95
unknown <yebkobvsebolshe@gmail.com>
2023-06-01 06:23:19 +0400

    states

--- src/App.js
+++ src/App.js
@@ -3,29 +3,38 @@ import Header from './components/Header'
 import Image from './components/Image'
 import logo from './img/logo.jpg'
 
-
-
 class App extends React.Component {
-  helpText = 'Help text!'
+  constructor(props) {
+    super(props)
+    this.state = {
+      helpText: 'Help text',
+      userData: ''
+    }
+
+    this.inputClick = this.inputClick.bind(this) // ЗАЛУПА
+  }
 
   render() {
     return (
       <div className='name'>
-        <Header title="Шапка сайта!!!"/>
-        <h1>{this.helpText}</h1>
+        <Header title='Шапка сайта!!!' />
+        <h1>{this.state.helpText}</h1>
+        <h2>{this.state.userData}</h2>
         <input
-          placeholder={this.helpText}
+          onChange={event => this.setState({ userData: event.target.value })}
+          placeholder={this.state.helpText}
           onClick={this.inputClick}
           onMouseEnter={this.mouseOver}
         />
-        <p>{this.helpText === 'Help text!' ? 'Yes' : 'No'}</p>
-        <Image image={logo}/>
-        <img src={logo}/>
+        <p>{this.state.helpText === 'Help text' ? 'Yes' : 'No'}</p>
+        <Image image={logo} />
+        <img src={logo} />
       </div>
     )
   }
 
   inputClick() {
+    this.setState({ helpText: 'Changed' })
     console.log('Clicked')
   }
   mouseOver() {
